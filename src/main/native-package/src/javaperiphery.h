84dac120df9c13aae25ec88a178224826157b131
servadmin <servadmin@ubuntuserv>
2021-11-14 19:11:45 -0500

    Add i2c_write16 to header

--- src/main/native-package/src/javaperiphery.h
+++ src/main/native-package/src/javaperiphery.h
@@ -13,6 +13,7 @@ extern "C" {
 int i2c_read8(i2c_t *i2c, uint16_t addr, uint16_t reg, uint8_t *buf, size_t len);
 int i2c_read16(i2c_t *i2c, uint16_t addr, uint16_t reg, uint8_t *buf, size_t len);
 int i2c_write8(i2c_t *i2c, uint16_t addr, uint16_t reg, uint16_t value);
+int i2c_write16(i2c_t *i2c, uint16_t addr, uint16_t reg, uint16_t value);
 
 #ifdef __cplusplus
 }
